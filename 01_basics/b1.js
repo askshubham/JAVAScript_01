3b71d5e6c1e20fd85127315ac762e75f7fb451ff
shubham kumar <askshubham@outlook.com>
2023-06-05 06:28:57 +0000

    c2

---
+++ 01_basics/b1.js
@@ -0,0 +1,14 @@
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;
+
+let a, b, c;
+a = 23;
+b = 24;
+c = a+b;
+console.log(z,c)
+
+let s, d, e;
+s=1; d=2, e=s+d;
+console.log(e);
